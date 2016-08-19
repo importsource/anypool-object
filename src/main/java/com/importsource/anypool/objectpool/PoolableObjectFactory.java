436f916e0a6a51f42178564f95cffb6be021a03c
importsource <hezhuofan24@126.com>
2016-08-19 23:25:43 +0800

    f

---
+++ src/main/java/com/importsource/anypool/objectpool/PoolableObjectFactory.java
@@ -0,0 +1,33 @@
+package com.importsource.anypool.objectpool;
+
+/**
+ * 池化对象管理工厂接口PoolableObjectFactory：用于管理对象池中对象的创建、还原、销毁
+ * 
+ * @author Hezf
+ */
+public interface PoolableObjectFactory
+{
+	/**
+	 * 生成新对象
+	 * 
+	 * @param clsType
+	 *            对象类型
+	 * @return 新对象
+	 */
+	public Object createObject(Class clsType);
+
+	/**
+	 * 对象在使用过程中内部状态会发生变化，当归还对象池时可能需要将对象还原为原始状态
+	 * 
+	 * @return 将对象还原为原始状态
+	 */
+	public Object clearObject(Object obj);
+
+	/**
+	 * 销毁一个对象
+	 * 
+	 * @param obj
+	 *            要销毁的对象
+	 */
+	public void destroyObject(Object obj);
+}
---
+++ src/main/java/com/importsource/anypool/objectpool/Test.java
@@ -0,0 +1,90 @@
+package com.importsource.anypool.objectpool;
+  
+/** 
+ * 对象池测试类Test 
+ *  
+ *  @author Hezf
+ */  
+public class Test  
+{  
+    public static void main(String[] args)  
+    {  
+        // 创建String类型的池化对象管理工厂  
+        PoolableObjectFactory factory = StringPoolableObjectFactory  
+                .getInstance();  
+  
+        // 创建栈式对象池  
+        ObjectPool pool = StackObjectPoolFactory.getInstance().createPool(  
+                factory, 100, String.class);  
+  
+        // 多线程测试  
+        for (int index = 1; index <= 20; ++index)  
+        {  
+            new MyThread(pool, factory).start();  
+        }  
+    }  
+}  
+  
+/** 
+ * 封装线程用于测试对象池 
+ *  
+ * Email:530025983@qq.com 
+ *  
+ * @author MONKEY.D.MENG 2011-03-16 
+ *  
+ */  
+class MyThread extends Thread  
+{  
+    /** 对象池 */  
+    private ObjectPool pool = null;  
+  
+    /** 池化对象管理工厂 */  
+    private PoolableObjectFactory factory = null;  
+  
+    /** 
+     *  
+     * @param pool 
+     *            对象池 
+     * @param factory 
+     *            池化对象管理工厂 
+     */  
+    public MyThread(ObjectPool pool, PoolableObjectFactory factory)  
+    {  
+        this.pool = pool;  
+        this.factory = factory;  
+    }  
+  
+    public void run()  
+    {  
+        // 获取新对象  
+        String buffer = (String) pool.borrowObject();  
+  
+        // 必要的业务逻辑神马的  
+        buffer = "【" + this.currentThread() + ":MONKEY.D.MENG】-->";  
+        System.out.println(buffer + "/t活跃对象数：" + pool.getActiveNum()  
+                + "/t空闲对象数：" + pool.getIdleNum());  
+  
+        // 返回对象  
+        pool.returnObject(factory.clearObject(buffer));  
+    }  
+  
+    public ObjectPool getPool()  
+    {  
+        return pool;  
+    }  
+  
+    public void setPool(ObjectPool pool)  
+    {  
+        this.pool = pool;  
+    }  
+  
+    public PoolableObjectFactory getFactory()  
+    {  
+        return factory;  
+    }  
+  
+    public void setFactory(PoolableObjectFactory factory)  
+    {  
+        this.factory = factory;  
+    }  
+}  
